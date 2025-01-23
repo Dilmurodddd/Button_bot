d1037c27467b1199a9820bea3ef3225abb2a25f3
dilmurod <dilmurod@dilmurod.dilmurod>
2025-01-23 16:41:48 +0500

    Button_bot

--- main.py
+++ main.py
@@ -11,33 +11,29 @@ def get_id():
     return  respond['result'][-1]['message']['chat']['id']
 
 def send_message(text, chat_id):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+
+
 
     keyboard_buttons = [
-        [{'text': 'Tugma1 '}], [{'text': 'Tugma2 '} 
-        ,{'text': 'Tugma3 '}]   
+       [{'text': 'Tugma1 '}], [{'text': 'Tugma2 '} 
+       ,{'text': 'Tugma3 '}]   
     ]
-
-
     reply_keyboard_markup = {
-        'keyboard': keyboard_buttons,
-        'resize_keyboard': True,
-        'one_time_keyboard': True
+       'keyboard': keyboard_buttons,
+       'resize_keyboard': True,
+       'one_time_keyboard': True
     }
-
     params = {
-        'chat_id': chat_id,
-        'text': text,
-        'reply_markup': json.dumps(reply_keyboard_markup), 
+       'chat_id': chat_id,
+       'text': text,
+       'reply_markup': json.dumps(reply_keyboard_markup), 
     }
-    
+   
     r = requests.get(url, params=params)
     return r.json()
 
-
-
 print(get_id())
 
-TEXT = 'Assalomu alaykum bu kurslarni sotish uchun tajriba bot\no\'sizga kerakli tugmani bosing va kerakli ma\'lumotlarni qo\'lga kiriting!!!'
-
-send_message(TEXT, get_id())
+chat_id = get_id()
+send_message('Assalomu alaykum bu kurslarni sotish uchun tajriba bot\no\'sizga kerakli tugmani bosing va kerakli ma\'lumotlarni qo\'lga kiriting!!!',chat_id)
